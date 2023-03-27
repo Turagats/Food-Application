7a2ec516b024f9122b09c0690f71a82b5740375a
Turagats <o.napetvaridze@gmail.com>
2023-03-27 18:45:32 +0400

    cart amount works properly

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -8,7 +8,7 @@ export default function Header(props) {
     <Fragment>
       <header className={classes.header}>
         <h1>Meals</h1>
-        <HeaderCartButton />
+        <HeaderCartButton onClick={props.onShowCart} />
       </header>
       <div className={classes["main-image"]}>
         <img src={mealsimage} alt="meals" />
